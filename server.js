c5064b125592e0fd1510cf18250456c86192badf
SamHeutmaker <samheutmaker@gmail.com>
2016-02-20 16:31:23 -0800

    First live version

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('build'));
 
-app.listen(3000, () => {
-  console.log('Server up on port 3000');
+app.listen(PORT, () => {
+  console.log('Server up on port ' + PORT);
 })
